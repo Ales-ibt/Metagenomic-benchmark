c33f7cb9fafe36108fd90ecd63dfb2f4bafc107c
Ales-ibt <ales@ibt.unam.mx>
2018-01-23 18:24:38 -0600

    Update qiime_transfer.pl

--- bin/16SrRNAamplicon/qiime_transfer.pl
+++ bin/16SrRNAamplicon/qiime_transfer.pl
@@ -3,8 +3,26 @@ use strict;
 use warnings;
 
 ######## PROGRAM THAT TRANSFERS THE INFORMATION FROM THE REPRESENTATIVE SEQUENCE TO ALL SEQUENCES IN THE OTU #######
-######## Alejandra Escobar Zepeda, USMB, IBt, UNAM
+######## Alejandra Escobar-Zepeda, USMB, IBt, UNAM
 ######## 08/Agosto/2017
+#  qiime_transfer.pl
+#  Copyright 2017- Alejandra Escobar-Zepeda (ales2688@gmail.com)
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#
 
 scalar @ARGV == 2 || die "usage: $0 <otus_list.txt> <reads_ranking.txt>
 ";
