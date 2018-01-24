05a7b1f3af3ea6bc67282eca4abcd2137f02aa3f
Ales-ibt <ales@ibt.unam.mx>
2018-01-23 18:26:45 -0600

    Update shuffled_fasta.pl

--- bin/16SrRNAamplicon/shuffled_fasta.pl
+++ bin/16SrRNAamplicon/shuffled_fasta.pl
@@ -5,6 +5,24 @@ use warnings;
 ###  PROGRAM THAT GENERATES SHUFFLED SEQUENCES FROM A FASTA FILE AND PARAMETERS OF K-MER SIZE AND STEP ###
 ###  Alejandra Escobar, UUSMB, IBt, UNAM
 ###  07/September/2017
+#  shuffled_fasta.pl
+#  Copyright 2017- Alejandra Escobar-Zepeda (ales2688@gmail.com)
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#
 
 scalar@ARGV == 3 || die "usage: $0 <file.fasta[file]> <kmer_size[int]> <step[int]>
 ";
@@ -13,7 +31,7 @@ my$file = $ARGV[0];
 my$kmer_size = $ARGV[1];
 my$step = $ARGV[2];
 
-## Abriendo el archivo fasta
+## Opening the fasta file
 open FASTAOUT, (">shuffled_kmer$kmer_size\_step$step.fasta") or die ("I cannot create the output file\n");
 my$id_seq;
 my@guide_rand=();
