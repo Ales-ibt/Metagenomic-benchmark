cf53c28f804154dcf52a9a360ffce42b9aded3aa
Tina Godoy <tinagodoy@gmail.com>
2018-01-29 13:13:55 -0600

    Update 7.subtit_TF.pl

--- bin/16SrRNAamplicon/7.subtit_TF.pl
+++ bin/16SrRNAamplicon/7.subtit_TF.pl
@@ -1,11 +1,24 @@
 #!/usr/bin/perl
+#  7.subtit.TF.pl
+#  Copyright 2017- Alejandra Escobar Zepeda, USMB, IBt, UNAM (ales2688@gmail.com)
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+
 use strict;
 use warnings;
-
-######## PROGRAMA QUE SUSTITUYE LOS NIVELES TAXONOMICOS INDEFINIDOS #######
-######## Para tablas de taxid de Tina del proyecto de benchmark
-######## Alejandra Escobar Zepeda, USMB, IBt, UNAM
-######## 27/Febrero/2017
 
 scalar@ARGV == 1 || die "usage: $0 <table.tab> 
 " ;
